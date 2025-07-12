6cb61d9508878a4b4efc61f9827589c13a4dd537
PeteR449-ux <sameer.gaidhani05@gmail.com>
2025-07-12 19:13:21 +0530

    Cleaned build and removed unwanted inject plugin

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  root: '.', // this tells Vite that the root is the current folder
   server: {
     port: 5173,
   },
